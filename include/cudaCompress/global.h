e09dc47f3036be30fb94d177b6d626f727dd4506
Kyle Brooks <brookskd@gmail.com>
2015-11-02 06:31:11 -0500

    Fixed CUCOMP_DLL export / import attributes to be cross platform.

--- include/cudaCompress/global.h
+++ include/cudaCompress/global.h
@@ -15,11 +15,36 @@ typedef unsigned long long int uint64;
 
 
 #if defined(CUCOMP_BUILD_STANDALONE)
-#define CUCOMP_DLL
+	#define CUCOMP_DLL
 #elif defined(CUCOMP_BUILD_DLL)
-#define CUCOMP_DLL __declspec(dllexport)
+	#if defined _WIN32 || defined __CYGWIN__
+		#ifdef __GNUC__
+			#define CUCOMP_DLL __attribute__ ((dllexport))
+		#else
+			#define CUCOMP_DLL __declspec(dllexport)
+		#endif
+	#else
+		#if __GNUC__ >= 4
+			#define CUCOMP_DLL __attribute__ ((visibility("default")))
+		#else
+			#define CUCOMP_DLL
+		#endif
+	#endif
 #else
-#define CUCOMP_DLL __declspec(dllimport)
+	#if defined _WIN32 || defined __CYGWIN__
+		#ifdef __GNUC__
+			#define CUCOMP_DLL __attribute__ ((dllimport))
+		#else
+			#define CUCOMP_DLL __declspec(dllimport)
+		#endif
+	#else
+		// Not sure if this is needed or not, better be safe and put it in just in case.
+		#if __GNUC__ >= 4
+			#define CUCOMP_DLL __attribute__ ((visibility("default")))
+		#else
+			#define CUCOMP_DLL
+		#endif
+	#endif
 #endif
 
 
